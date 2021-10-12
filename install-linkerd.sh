63f3e69ebd1f7dd3151f718b18085a63e1adec98
Israel Miller <ismille@microsoft.com>
2021-10-12 18:37:17 -0400

    fixing typos

--- install-linkerd.sh
+++ install-linkerd.sh
@@ -3,9 +3,9 @@ set -e
 curl -sL run.linkerd.io/install | sh 
 export PATH=$PATH:/home/runner/.linkerd2/bin
 linkerd version
-export LINKERD_PRECHECK = $(linkerd check --pre -o short)
+export LINKERD_PRECHECK=$(linkerd check --pre -o short)
 
-if [[$LINKERD_PRECHECK == *"linkerd upgrade"*]]; then
+if [[ $LINKERD_PRECHECK == *"linkerd upgrade"* ]]; then
     linkerd upgrade | kubectl apply -f -
     linkerd upgrade | kubectl apply --prune -l linkerd.io/control-plane-ns=linkerd \
   --prune-whitelist=rbac.authorization.k8s.io/v1/clusterrole \
