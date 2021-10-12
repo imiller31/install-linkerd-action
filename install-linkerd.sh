362945e5719b4a6d64f20f4be58729de5b920cb0
Israel Miller <ismille@microsoft.com>
2021-10-12 18:07:48 -0400

    fixing typo

--- install-linkerd.sh
+++ install-linkerd.sh
@@ -3,7 +3,7 @@ export PATH=$PATH:/home/runner/.linkerd2/bin
 linkerd version
 linkerd check --pre
 linkerd install | kubectl --kubeconfig $KUBECONFIG apply -f -
-if [$INSTALL_VIZ}] 
+if [$INSTALL_VIZ] 
 then 
     linkerd viz install | kubectl --kubeconfig $KUBECONFIG apply -f - 
 fi
