141b294793dc30705ce6f3bcc16cb85e1b73427f
Israel Miller <ismille@microsoft.com>
2021-10-12 17:29:17 -0400

    adding env

--- install-linkerd.sh
+++ install-linkerd.sh
@@ -2,9 +2,9 @@ curl -sL run.linkerd.io/install | sudo bash
 export PATH=$PATH:/home/runner/.linkerd2/bin
 linkerd version
 linkerd check --pre
-linkerd install | kubectl --kubeconfig ${{ inputs.KUBECONFIG}} apply -f -
-if [${INSTALL_VIZ}] 
+linkerd install | kubectl --kubeconfig $KUBECONFIG apply -f -
+if [$INSTALL_VIZ] 
 then 
-    linkerd viz install | kubectl --kubeconfig ${{ inputs.KUBECONFIG}} apply -f - 
+    linkerd viz install | kubectl --kubeconfig $KUBECONFIG apply -f - 
 fi
 linkerd check
