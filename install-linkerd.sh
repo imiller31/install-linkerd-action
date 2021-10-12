797f19702dafef11053c3bc07a0086b2054bad19
Israel Miller <ismille@microsoft.com>
2021-10-12 17:47:39 -0400

    remove sudo

--- install-linkerd.sh
+++ install-linkerd.sh
@@ -1,9 +1,9 @@
-curl -sL run.linkerd.io/install | sudo bash    
-export PATH=$PATH:/root/.linkerd2/bin
+curl -sL run.linkerd.io/install | sh 
+export PATH=$PATH:/home/runner/.linkerd2/bin
 linkerd version
 linkerd check --pre
 linkerd install | kubectl --kubeconfig $KUBECONFIG apply -f -
-if [$INSTALL_VIZ] 
+if [$INSTALL_VIZ}] 
 then 
     linkerd viz install | kubectl --kubeconfig $KUBECONFIG apply -f - 
 fi
