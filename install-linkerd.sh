f08f415197a7c30d98f9b14853ddcd796fda05ff
Israel Miller <ismille@microsoft.com>
2021-10-13 15:24:10 -0400

    fixing upgrade

--- install-linkerd.sh
+++ install-linkerd.sh
@@ -6,11 +6,7 @@ linkerd version
 export LINKERD_PRECHECK=$(linkerd check --pre -o short 2>&1)
 
 if grep -q "linkerd upgrade" <<< "$LINKERD_PRECHECK"; then
-    linkerd upgrade | kubectl apply --prune -l linkerd.io/control-plane-ns=linkerd -f -
-    linkerd upgrade | kubectl apply --prune -l linkerd.io/control-plane-ns=linkerd \
-  --prune-whitelist=rbac.authorization.k8s.io/v1/clusterrole \
-  --prune-whitelist=rbac.authorization.k8s.io/v1/clusterrolebinding \
-  --prune-whitelist=apiregistration.k8s.io/v1/apiservice -f -
+    linkerd upgrade | kubectl apply -f -
 else
     linkerd install config | kubectl apply -f -
     linkerd check config
